56fe952ceaec024fd352b60a4c8c7c844928b314
xiaoqiang <756164814@qq.com>
2020-07-03 10:29:07 +0800

    增加404处理

--- netty-http/http-static-server/src/main/java/com/xiaoqiang/hss/StaticResourceDecoder.java
+++ netty-http/http-static-server/src/main/java/com/xiaoqiang/hss/StaticResourceDecoder.java
@@ -4,6 +4,7 @@ import com.xiaoqiang.netty.http.core.HttpRequest;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 public class StaticResourceDecoder extends MessageToMessageDecoder<HttpRequest> {
@@ -11,7 +12,13 @@ public class StaticResourceDecoder extends MessageToMessageDecoder<HttpRequest> 
 
     @Override
     protected void decode(ChannelHandlerContext ctx, HttpRequest msg, List<Object> out) throws Exception {
-        StaticResource resource = new StaticResource(msg.getUri());
+        StaticResource resource;
+        try {
+            resource = new StaticResource(msg.getUri());;
+
+        } catch (Exception e){
+            resource = new StaticResource("/404.html");
+        }
         resource.setRequest(msg);
         ctx.writeAndFlush(resource);
         ctx.close();
